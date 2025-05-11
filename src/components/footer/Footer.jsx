b4b4daeafaff973927b815838ce3e94b672a2f79
MuxtasarOtaxanova <otaxanovamuxtasar33@gmail.com>
2025-05-11 21:16:17 +0500

    Portfolio

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,13 @@
+import React, { useState } from "react";
 import "./Footer.css";
+
 const Footer = () => {
+  const [email, setEmail] = useState("");
+
+  const handleConnectClick = () => {
+    setEmail(""); // Email inputni tozalaydi
+  };
+
   return (
     <div id="footer" className="footer">
       <div className="footer-top">
@@ -14,7 +22,12 @@ const Footer = () => {
         </div>
         <div className="footer-top-right">
           <div className="footer-email-input">
-            <input type="email" placeholder="Enter your email" />
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="footer-subscribe">Subscribe</div>
         </div>
@@ -27,7 +40,9 @@ const Footer = () => {
         <div className="footer-bottom-right">
           <p>Term of services</p>
           <p>Privacy Policy</p>
-          <p>Connect with me</p>
+          <p onClick={handleConnectClick} style={{ cursor: "pointer" }}>
+            Connect with me
+          </p>
         </div>
       </div>
     </div>
